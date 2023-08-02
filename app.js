0c03b045fc38991bd788bb49b90ec7ffd3dfc80d
GeonLegend <julianleandroded5051@gmail.com>
2023-08-02 19:06:11 -0300

    Micro desafio - Paso I Y II - Clase 21

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
-app.listen(PORT, () => console.log('Escuchando el el puerto ' + PORT))
+
+app.post('/register', (req, res) =>{
+    res.redirect('/');
+})
+app.post('/login', (req,res) => {
+    res.redirect('/');
+})
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.listen(PORT, () => console.log('Escuchando el el puerto ' + PORT));
